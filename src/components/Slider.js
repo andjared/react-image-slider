de2ccbefa2f195f26db3a9b7e240aa0fd9e6084b
andja <andja.velimirovic@gmail.com>
2022-11-25 20:53:15 +0100

    thumbnail onclick

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaExpand } from "react-icons/fa";
 import data from "../images/data";
 
 const Slider = () => {
+  const [imgIndex, setImgIndex] = useState(0);
   return (
     <div className="slider">
       <div className="image-wrapper">
         <div className="image-order">
           <span>1 / 20</span>
         </div>
-        <img src={data[0].url} alt="" />
+        <img src={data[imgIndex].url} alt={data[imgIndex].url} />
         <div className="btn-zoom">
-          <button>zoom</button>
+          <button>
+            <FaExpand />
+          </button>
         </div>
       </div>
       <div className="thumbnails">
-        {data.map((item) => {
-          return <img src={item.url} alt={item.url} key={item.id} />;
+        {data.map((item, index) => {
+          return (
+            <img
+              src={item.url}
+              alt={item.url}
+              key={item.id}
+              onClick={() => setImgIndex(index)}
+            />
+          );
         })}
       </div>
     </div>
